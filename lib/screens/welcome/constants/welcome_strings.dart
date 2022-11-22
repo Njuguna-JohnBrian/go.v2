aa5fa65d1d6ac86648f7b44290d854fd1a070a58
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-22 17:44:40 +0300

    Create welcome string constants

--- lib/screens/welcome/constants/welcome_strings.dart
+++ lib/screens/welcome/constants/welcome_strings.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
-class WelcomeStrings{}
+class WelcomeStrings {
+  static const seeWhatsHappening =
+      "See what's happening in the world of travel right now";
+  static const continueToLogin = "Continue to Login";
+  static const dontHaveAnAccount = "Don't have an account?";
+  static const createAnAccount = "Create an account";
+
+  const WelcomeStrings._();
+
+}
