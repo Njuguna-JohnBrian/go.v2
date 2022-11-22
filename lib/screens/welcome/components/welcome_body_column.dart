9c28bd42641d2290618686389ca4f6ce57c321d6
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-22 18:29:58 +0300

    Create welcome body component

---
+++ lib/screens/welcome/components/welcome_body_column.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go/screens/screens_barrel.dart';
+import 'package:go/theme/go_theme.dart';
+
+class WelcomeBodyColumn extends StatelessWidget {
+  const WelcomeBodyColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: size.height * 0.25,
+        ),
+        SvgPicture.asset(
+          WelcomeAssets.goSvgLogo,
+          height: 100,
+        ),
+        Text(
+          WelcomeStrings.seeWhatsHappening,
+          style: GoTheme.lightTextTheme.headline1,
+        ),
+        SizedBox(
+          height: size.height * 0.03,
+        ),
+        const WelcomeButton(),
+        const Spacer(),
+        Text(WelcomeStrings.dontHaveAnAccount,
+            style: GoTheme.lightTextTheme.headline2),
+        Text(
+          WelcomeStrings.createAnAccount,
+          style: GoTheme.lightTextTheme.headline6?.copyWith(color: Colors.blue),
+        ),
+      ],
+    );
+  }
+}
