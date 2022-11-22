97792ce0a96382baabd2546f96de4e6562e84bfb
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-22 18:29:19 +0300

    export column and body

--- lib/screens/welcome/welcome_barrel.dart
+++ lib/screens/welcome/welcome_barrel.dart
@@ -2,3 +2,5 @@ export 'welcome.dart';
 export 'constants/welcome_strings.dart';
 export 'constants/welcome_assets.dart';
 export 'components/welcome_body.dart';
+export 'components/welcome_button.dart';
+export 'components/welcome_body_column.dart';
