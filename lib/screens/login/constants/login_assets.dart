2506d040913d8af0329deef0e6b261b0b82a46d6
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-25 15:03:59 +0300

    Create constant strings

---
+++ lib/screens/login/constants/login_assets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+
+@immutable
+class LoginAssets{
+
+  static const logoSvg = "assets/svgs/logo.svg";
+  static const googleLogo = "assets/images/google_logo.png";
+  static const facebookLogo = "assets/images/facebook.png";
+  const LoginAssets._();
+}
---
+++ lib/screens/login/constants/login_strings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class LoginStrings {
+  static const loginToGo = "Login to go!";
+  static const continueWithFaceBook = "Continue with Facebook";
+
+  static const continueWithGoogle = "Continue with Google";
+  static const enterEmail = "Enter email";
+
+  static const enterPassword = "Enter password";
+  static const login = "Login";
+
+  static const forgotPassword = "Forgot Password?";
+  static const dontHaveAnAccount = "Don't have an account?";
+
+  static const createAccount = "Create an account";
+  static const or = "OR";
+
+  const LoginStrings._();
+}
