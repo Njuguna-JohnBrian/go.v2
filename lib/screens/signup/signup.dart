4596451acb5c00ac6cb051de5d146275b1c17b55
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-26 13:41:14 +0300

    create sign up screen

---
+++ lib/screens/signup/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:go/screens/signup/signup_barrel.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SignUpBody(),
+    );
+  }
+}
