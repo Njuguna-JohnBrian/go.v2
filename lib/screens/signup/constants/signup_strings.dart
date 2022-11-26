90092a01a86698067ac6a2cadc093561fdcc3d30
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-26 13:43:30 +0300

    create signup strings class

---
+++ lib/screens/signup/constants/signup_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class SignUpStrings {
+  static const newAccount = "New go Account!";
+  static const enterName = "Enter name";
+
+  static const enterEmail = "Enter email";
+  static const enterPassword = "Enter password";
+
+  static const confirmPassword = "Confirm password";
+  static const createAccount = "Create Account";
+
+  static const alreadyHaveAccount = "Already have an account?";
+  static const login = "Login";
+
+  const SignUpStrings._();
+}
