4cd8f0adc5ac1f0e9aa4683615774b3caefa78d4
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-26 12:14:04 +0300

    create containing class for password strings

---
+++ lib/screens/password/constants/password_strings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class PasswordStrings{
+
+  static const forgotPassword = "Forgot Password?";
+  static const receiveLink = "Please enter the email address \n linked with your account";
+  static const enterEmail = "Enter email";
+
+  static const getResetLink = "Get password reset link";
+  static const rememberPassword = "Remember Password?";
+  static const login = "Login";
+
+  const PasswordStrings._();
+}
