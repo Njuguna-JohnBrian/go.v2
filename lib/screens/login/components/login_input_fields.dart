ebe3ab0dba04183945af781152611be4753ed8a1
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-11-29 16:21:36 +0300

    alter login ui

--- lib/screens/login/components/login_input_fields.dart
+++ lib/screens/login/components/login_input_fields.dart
@@ -1,32 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import './login_input_container.dart';
+import '../../../globals/global_assets.dart';
+import '../constants/login_strings.dart';
 
-class LoginTextInputField extends StatelessWidget {
+class LoginEmailInputField extends StatelessWidget {
   final TextEditingController textEditingController;
-  final IconData icon;
-  final String hintText;
-  const LoginTextInputField({
+
+  const LoginEmailInputField({
     Key? key,
-    required this.icon,
-    required this.hintText,
     required this.textEditingController,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LoginTextFieldContainer(
-      child: TextField(
-        autofillHints: const [AutofillHints.email],
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.only(
+        left: size.width * 0.10,
+        right: size.width * 0.10,
+      ),
+      child: TextFormField(
         controller: textEditingController,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        cursorColor: Colors.grey,
+        validator: ((value) {
+          if (value!.isEmpty) {
+            return "Email cannot be empty";
+          } else if (!RegExp(GlobalAssets.emailPattern).hasMatch(value)) {
+            return GlobalAssets.invalidEmailMessage;
+          } else {
+            return null;
+          }
+        }),
+        keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
-          hintStyle: GoogleFonts.openSans(),
-          icon: Icon(
-            icon,
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          prefixIcon: Icon(
+            Icons.email,
             color: Colors.grey.shade400,
           ),
-          hintText: hintText,
-          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          labelText: LoginStrings.enterEmail,
+          labelStyle: GoogleFonts.openSans().copyWith(
+            color: Colors.grey.shade500,
+          ),
         ),
       ),
     );
--- lib/screens/login/components/login_input_password.dart
+++ lib/screens/login/components/login_input_password.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../../globals/global_assets.dart';
+import '../constants/login_strings.dart';
 import './login_input_container.dart';
 
 class LoginPasswordInputField extends StatefulWidget {
-  final TextEditingController? textEditingController;
-  final IconData icon;
-  final String hintText;
+  final TextEditingController passwordController;
   const LoginPasswordInputField({
     Key? key,
-    required this.icon,
-    required this.hintText,
-    this.textEditingController,
+    required this.passwordController,
   }) : super(key: key);
 
   @override
@@ -34,19 +32,32 @@ class _LoginPasswordInputFieldState extends State<LoginPasswordInputField> {
 
   @override
   Widget build(BuildContext context) {
-    return LoginTextFieldContainer(
-      child: TextField(
-        controller: widget.textEditingController,
-        autofillHints: const [AutofillHints.password],
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.only(
+        left: size.width * 0.10,
+        right: size.width * 0.10,
+      ),
+      child: TextFormField(
         obscureText: !showPassword,
+        controller: widget.passwordController,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        cursorColor: Colors.grey,
+        validator: ((value) {
+          if (value!.isEmpty) {
+            return "Password cannot be empty";
+          } else if (value.length < 8) {
+            return GlobalAssets.passwordTooShortMessage;
+          } else if (!RegExp(GlobalAssets.passwordPattern).hasMatch(value)) {
+            return GlobalAssets.invalidPasswordMessage;
+          } else {
+            return null;
+          }
+        }),
+        keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
-          hintStyle: GoogleFonts.openSans(),
-          icon: Icon(
-            widget.icon,
-            color: Colors.grey.shade400,
-          ),
-          hintText: widget.hintText,
-          border: InputBorder.none,
+          fillColor: Colors.grey.shade200,
+          filled: true,
           suffixIcon: IconButton(
             onPressed: () => setState(() {
               showPassword = !showPassword;
@@ -55,6 +66,38 @@ class _LoginPasswordInputFieldState extends State<LoginPasswordInputField> {
               showPassword ? Icons.visibility : Icons.visibility_off,
               color: Colors.grey.shade400,
             ),
+          ),
+          prefixIcon: Icon(
+            Icons.password,
+            color: Colors.grey.shade400,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade200,
+            ),
+          ),
+          labelText: LoginStrings.enterPassword,
+          labelStyle: GoogleFonts.openSans().copyWith(
+            color: Colors.grey.shade500,
           ),
         ),
       ),
