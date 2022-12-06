8c4c8d6d6d30bf5f8853babfd9c505914c9d19dd
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-12-07 02:18:15 +0300

    setup profile screen user data

--- lib/screens/profile/components/profile_body.dart
+++ lib/screens/profile/components/profile_body.dart
@@ -10,7 +10,7 @@ class ProfileBody extends StatelessWidget {
       CustomScrollView(
       slivers: [
         ProfileSliverAppBar(),
-        // ProfileSliverBody()
+        ProfileSliverBody()
       ],
     );
   }
