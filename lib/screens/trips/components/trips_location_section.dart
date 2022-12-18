953a979622e658da048ed2833224822a8ceb143c
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-12-18 17:16:03 +0300

    add location functionality

--- lib/screens/trips/components/trips_location_section.dart
+++ lib/screens/trips/components/trips_location_section.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 import 'package:go/theme/go_theme.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
-
-class TripsLocationSection extends StatelessWidget {
+class TripsLocationSection extends StatefulWidget {
   const TripsLocationSection({Key? key}) : super(key: key);
+
+  @override
+  State<TripsLocationSection> createState() => _TripsLocationSectionState();
+}
+
+class _TripsLocationSectionState extends State<TripsLocationSection> {
+  final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+  final TextEditingController locationController = TextEditingController();
+  bool appHasLocationPermission = false;
+  bool isGlobalLocationEnabled = false;
+  late String currentAddress;
+
+  @override
+  void initState() {
+    _checkLocationOnInit();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    locationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +46,15 @@ class TripsLocationSection extends StatelessWidget {
         SizedBox(
           width: size.width * 0.45,
           child: TextFormField(
+            controller: locationController,
+            autovalidateMode: AutovalidateMode.always,
+            validator: ((value) {
+              if (value!.isEmpty) {
+                return "Please click to enable location";
+              } else {
+                return null;
+              }
+            }),
             onTap: () {},
             readOnly: true,
             decoration: InputDecoration(
@@ -34,8 +67,70 @@ class TripsLocationSection extends StatelessWidget {
         SizedBox(
           width: size.width * 0.16,
         ),
-        Switch(value: false, onChanged: (value) {})
+        Switch(
+          value: isGlobalLocationEnabled,
+          onChanged: (value) {
+            _handlePermission();
+          },
+        ),
       ],
     );
+  }
+
+  _checkLocationOnInit() async {
+    bool globalLocationServiceEnabled =
+        await _geolocatorPlatform.isLocationServiceEnabled();
+    setState(() {
+      isGlobalLocationEnabled = globalLocationServiceEnabled;
+    });
+
+    if (globalLocationServiceEnabled == true) {
+      await _getCurrentLocation();
+    }
+  }
+
+  _handlePermission() async {
+    bool globalLocationServiceEnabled =
+        await _geolocatorPlatform.isLocationServiceEnabled();
+    LocationPermission appLocationServiceEnabled =
+        await _geolocatorPlatform.checkPermission();
+    if (globalLocationServiceEnabled == false) {
+      final settingsResult = await _openAppLocationSettings();
+      await _getCurrentLocation();
+      setState(() {
+        isGlobalLocationEnabled = settingsResult;
+      });
+    }
+
+    if (appLocationServiceEnabled == LocationPermission.denied) {
+      await _enableAppLocationAccess();
+    } else if (appLocationServiceEnabled == LocationPermission.deniedForever) {
+      await _enableAppLocationAccess();
+    }
+  }
+
+  Future<bool> _openAppLocationSettings() async {
+    bool isEnabled = await _geolocatorPlatform.openLocationSettings();
+    return isEnabled;
+  }
+
+  Future<LocationPermission> _enableAppLocationAccess() async {
+    final appAccessEnabled = await _geolocatorPlatform.requestPermission();
+
+    return appAccessEnabled;
+  }
+
+  _getCurrentLocation() async {
+    await _handlePermission();
+    final position = await _geolocatorPlatform.getCurrentPosition();
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark place = placemarks[0];
+
+    setState(() {
+      currentAddress = "${place.locality}, ${place.country}";
+
+      locationController.text = currentAddress;
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
+import geolocator_apple
 import path_provider_macos
 import sqflite
 
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
