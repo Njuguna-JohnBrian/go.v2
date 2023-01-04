1c9aa850c3e45d669d5c39d528c22af4b9907162
Njuguna-JohnBrian <njugunajb96@gmail.com>
2023-01-04 05:31:02 +0300

    show and hide loading screen instance

---
+++ lib/screens/loading/loading_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go/screens/loading/loading_screen_controller.dart';
+import 'package:go/screens/loading/loading_strings.dart';
+
+
+class LoadingScreen {
+  LoadingScreen._sharedInstance();
+
+  static final LoadingScreen _shared = LoadingScreen._sharedInstance();
+
+  factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController? _controller;
+
+
+  void show({
+  required BuildContext context,
+    String text = LoadingStrings.loading,
+}){
+    if(_controller?.update(text) ?? false){
+      return;
+    }else{
+
+    //show overlay
+    }
+  }
+
+  void hide(){
+    _controller?.close();
+    _controller = null;
+  }
+
+
+}
