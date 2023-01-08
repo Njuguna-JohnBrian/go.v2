cd346931506b215c1b636724476dc540f77c63e0
Njuguna-JohnBrian <njugunajb96@gmail.com>
2023-01-08 15:28:59 +0300

    refactor user profile view

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:go/animations/anims/empty_content_animation_view.dart';
+import 'package:go/animations/anims/error_animation_view.dart';
+import 'package:go/animations/anims/loading_animation_view.dart';
 import 'package:go/screens/profile/profile_barrel.dart' show ProfileBody;
+import 'package:go/state/providers/userdata/userdata_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+class ProfileScreen extends ConsumerWidget {
+  const ProfileScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ProfileBody(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userData = ref.watch(userDataProvider);
+    return RefreshIndicator(
+      child: userData.when(
+        data: (userData) {
+          if (userData.isEmpty) {
+            return const EmptyContentAnimationView();
+          } else {
+            return const LoadingAnimationView();
+          }
+        },
+        error: (error, stackTrace) {
+          return const ErrorAnimationView();
+        },
+        loading: () {
+          return const LoadingAnimationView();
+        },
+      ),
+      onRefresh: () {
+        ref.refresh(userDataProvider);
+        return Future.delayed(
+          const Duration(
+            seconds: 1,
+          ),
+        );
+      },
     );
   }
 }
