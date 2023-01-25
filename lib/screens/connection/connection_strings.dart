1924f0b4b01746407455d876d9460f8f89c1a070
John Brian <njugunajb96@gmail.com>
2023-01-25 05:04:08 +0300

    create connection strings

---
+++ lib/screens/connection/connection_strings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class ConnectionStrings {
+  static const noInternet = "No Internet Connection . . .";
+  const ConnectionStrings._();
+}
