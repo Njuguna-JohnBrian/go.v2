ded13cc2fd883c5a6cb0ac986916521e029f58e8
John Brian <njugunajb96@gmail.com>
2023-01-25 03:25:03 +0300

    add connection animation

--- lib/animations/anims/connection.dart
+++ lib/animations/anims/connection.dart
@@ -1,8 +1,8 @@
 import 'package:go/animations/anims/lottie_animation_view.dart';
 import 'package:go/animations/lottie_animation.dart';
 
-class NoConnection extends LottieAnimationView {
-  const NoConnection({
+class NoConnectionView extends LottieAnimationView {
+  const NoConnectionView({
     super.key,
   }) : super(animation: LottieAnimation.connection);
 }
