94723ebe9afbf6a26ead58a5c74e3d4b10c6f1b0
John Brian <njugunajb96@gmail.com>
2023-01-25 04:47:50 +0300

    remove misc

--- lib/screens/profile/constants/profile_assets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart' show immutable;
-
-
-@immutable
-class ProfileAssets{
-
-  static const worldMap = "assets/images/world_map.jpg";
-  const ProfileAssets._();
-}
--- lib/screens/view/constants/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart' show immutable;
-
-@immutable
-class ViewStrings {
-  static const noPostsAvailable =
-      'Nobody seems to have made any trip yet. Why don\'t you take the first step and upload your first trip!';
-  const ViewStrings._();
-}
