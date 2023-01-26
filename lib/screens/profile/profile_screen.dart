8cc53bb3e6c178b0d1ec9c2e771b76870e353637
John Brian <njugunajb96@gmail.com>
2023-01-26 22:48:37 +0300

    create profile widgets

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go/animations/anims/empty_content_animation_view.dart';
+import 'package:go/animations/anims/error_animation_view.dart';
+import 'package:go/animations/anims/loading_animation_view.dart';
+import 'package:go/state/state_barrel.dart' show userDataProvider;
+import 'package:go/theme/go_theme.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProfileScreen extends ConsumerWidget {
@@ -8,8 +13,34 @@ class ProfileScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      color: Colors.deepOrangeAccent,
+    final userData = ref.watch(userDataProvider);
+    return RefreshIndicator(
+      backgroundColor: GoTheme.mainColor,
+      color: Colors.white,
+      onRefresh: () {
+        return Future.delayed(
+          const Duration(
+            seconds: 3,
+          ),
+        );
+      },
+      child: userData.when(
+        data: (userData) {
+          if (userData.isEmpty) {
+            return const EmptyContentAnimationView();
+          } else {
+            return const Center(
+              child: LoadingAnimationView(),
+            );
+          }
+        },
+        error: (error, stackTrace) {
+          return const ErrorAnimationView();
+        },
+        loading: () {
+          return const LoadingAnimationView();
+        },
+      ),
     );
   }
 }
--- lib/state/providers/providers_barrel.dart
+++ lib/state/providers/providers_barrel.dart
@@ -3,3 +3,4 @@ export 'auth/is_logged_in_provider.dart';
 export 'auth/user_id_provider.dart';
 export 'connection/connection_provider.dart';
 export 'auth/isloading_provider.dart';
+export 'userdata/userdata_provider.dart';
