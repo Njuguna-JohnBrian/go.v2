fbd9a91f1841d2a467511942be534b8ff8bb10fd
John Brian <njugunajb96@gmail.com>
2023-01-26 20:39:24 +0300

    update home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:go/theme/go_theme.dart';
 
-import '../../globals/components/global_screens.dart';
-import '../../globals/components/global_spinner.dart';
+import 'package:go/globals/components/global_screens.dart' show goPages;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,17 +11,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isLoading = false;
   int pageIndex = 0;
   @override
   void initState() {
     super.initState();
-    isLoading = false;
-    Timer(const Duration(seconds: 0), () {
-      setState(() {
-        isLoading = false;
-      });
-    });
   }
 
   void _onTapped(int index) {
@@ -35,47 +25,43 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? GlobalSpinner(
-            context: context,
-          )
-        : Scaffold(
-            bottomNavigationBar: BottomNavigationBar(
-              onTap: _onTapped,
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              type: BottomNavigationBarType.shifting,
-              selectedItemColor: GoTheme.mainColor,
-              unselectedItemColor: Colors.grey.shade700,
-              unselectedFontSize: 8,
-              currentIndex: pageIndex,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.home,
-                  ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.search,
-                  ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.people_outline_outlined,
-                  ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.person_pin,
-                  ),
-                  label: '',
-                ),
-              ],
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTapped,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        type: BottomNavigationBarType.shifting,
+        selectedItemColor: GoTheme.mainColor,
+        unselectedItemColor: Colors.grey.shade700,
+        unselectedFontSize: 8,
+        currentIndex: pageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
             ),
-            body: goPages[pageIndex],
-          );
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.people_outline_outlined,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_pin,
+            ),
+            label: '',
+          ),
+        ],
+      ),
+      body: goPages[pageIndex],
+    );
   }
 }
