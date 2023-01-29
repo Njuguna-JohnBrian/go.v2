3ca68e2b74c0fdb140a8775ec398c506c527994c
John Brian <njugunajb96@gmail.com>
2023-01-30 01:00:23 +0300

    create profile strings

---
+++ lib/screens/profile/utils/strings.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+
+@immutable
+class ProfileStrings{
+
+  static const worldMap = "assets/images/world_map.jpg";
+  const ProfileStrings._();
+}
