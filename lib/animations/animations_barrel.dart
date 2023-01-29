f016a4b921433f12c6099fef3db1f8ee2b0cb804
John Brian <njugunajb96@gmail.com>
2023-01-30 00:39:10 +0300

    export anims

---
+++ lib/animations/animations_barrel.dart
@@ -0,0 +1,8 @@
+export 'anims/connection.dart';
+export 'anims/data_not_found_animation_view.dart';
+export 'anims/empty_content_animation_view.dart';
+export 'anims/empty_contents_wih_text_animation_view.dart';
+export 'anims/error_animation_view.dart';
+export 'anims/loading_animation_view.dart';
+export 'anims/lottie_animation_view.dart';
+export 'anims/small_error_animation_view.dart';
