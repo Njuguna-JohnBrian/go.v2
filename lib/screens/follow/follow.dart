f45cfe34a0a0953b03e36337368102ba0aea3504
John Brian <njugunajb96@gmail.com>
2023-02-05 10:39:32 +0300

    create follow screen app bar

--- lib/screens/follow/follow.dart
+++ lib/screens/follow/follow.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go/theme/go_theme.dart';
 
-
-class FollowScreen extends StatelessWidget {
+class FollowScreen extends StatefulWidget {
   const FollowScreen({Key? key}) : super(key: key);
 
   @override
+  State<FollowScreen> createState() => _FollowScreenState();
+}
+
+class _FollowScreenState extends State<FollowScreen> {
+  @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      backgroundColor: Colors.yellowAccent,
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            size: size.height * 0.05,
+            color: GoTheme.mainColor,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
     );
   }
 }
