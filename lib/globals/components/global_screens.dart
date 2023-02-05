f1a6d38a3fda8568965127388e996535aaa457dd
John Brian <njugunajb96@gmail.com>
2023-02-05 10:39:42 +0300

    change screen order

--- lib/globals/components/global_screens.dart
+++ lib/globals/components/global_screens.dart
@@ -3,8 +3,8 @@ import "package:go/screens/screens_barrel.dart"
     show ProfileScreen, FollowScreen;
 
 List<Widget> goPages = <Widget>[
-  const ProfileScreen(),
   const FollowScreen(),
+  const ProfileScreen(),
   Container(
     color: Colors.greenAccent,
   ),
