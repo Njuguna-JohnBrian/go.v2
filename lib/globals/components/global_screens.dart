60d1497b13ae23b1a9917efe5e7e2758f27dc41c
John Brian <njugunajb96@gmail.com>
2023-02-05 11:42:09 +0300

    render proper screens

--- lib/globals/components/global_screens.dart
+++ lib/globals/components/global_screens.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import "package:go/screens/screens_barrel.dart"
-    show ProfileScreen, FollowScreen;
+import "package:go/screens/screens_barrel.dart" show ProfileScreen;
 
 List<Widget> goPages = <Widget>[
-  const FollowScreen(),
   const ProfileScreen(),
+  Container(
+    color: Colors.redAccent,
+  ),
   Container(
     color: Colors.greenAccent,
   ),
