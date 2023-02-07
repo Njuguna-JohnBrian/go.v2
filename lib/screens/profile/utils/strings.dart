5ebf429e57daec7029b3100118010e87cfff651c
John Brian <njugunajb96@gmail.com>
2023-02-07 20:19:34 +0300

    create profile strings class

--- lib/screens/profile/utils/strings.dart
+++ lib/screens/profile/utils/strings.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/foundation.dart' show immutable;
 
-
 @immutable
-class ProfileStrings{
-
+class ProfileStrings {
   static const worldMap = "assets/images/world_map.jpg";
+
+  static const insights = "Insights";
+
+  static const trips = "Trips";
+
   const ProfileStrings._();
 }
