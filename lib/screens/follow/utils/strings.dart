95b8bad9b907bc86630f38711fb4059dd00d197d
John Brian <njugunajb96@gmail.com>
2023-02-07 20:19:25 +0300

    create strings class

---
+++ lib/screens/follow/utils/strings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class FollowStrings {
+  static const following = "following";
+
+  static const followers = "followers";
+
+  static const follower = "follower";
+
+  static const unfollow = "Unfollow";
+
+  static const remove = "Remove";
+
+
+  const FollowStrings._();
+}
