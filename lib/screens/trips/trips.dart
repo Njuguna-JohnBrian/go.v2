b9236e68846a496e3a6bd0014882de438acfeadf
John Brian <njugunajb96@gmail.com>
2023-02-08 12:44:42 +0300

    create app bar and app bar actions

--- lib/screens/trips/trips.dart
+++ lib/screens/trips/trips.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:go/theme/go_theme.dart';
 
 class TripsScreen extends StatelessWidget {
   const TripsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "New trip",
+          style: GoTheme.lightTextTheme.headline6?.copyWith(
+            color: GoTheme.mainColor,
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.chevron_left,
+            color: GoTheme.mainColor,
+            size: 35,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(
+              right: size.width * 0.045,
+            ),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "Save",
+                  style: GoTheme.lightTextTheme.headline6
+                      ?.copyWith(color: GoTheme.mainColor),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
