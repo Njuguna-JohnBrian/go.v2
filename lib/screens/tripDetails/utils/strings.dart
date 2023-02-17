011a6ae016faeeaeb683dc32e0783dd933fc2042
John Brian <njugunajb96@gmail.com>
2023-02-17 09:26:08 +0300

    trip details strings

---
+++ lib/screens/tripDetails/utils/strings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class TripDetailsStrings {
+  static const svgLogo = "assets/svgs/logo.svg";
+
+  static const userName = "John Brian";
+
+  static const decodedLocation = "Nyeri Kenya";
+
+  static const tripDescription =
+      "A trip along the Himalaya mountains across the Indies";
+
+  static const viewComments = "View Comments";
+
+  static const trackTrip = "Track Trip";
+
+  const TripDetailsStrings._();
+}
