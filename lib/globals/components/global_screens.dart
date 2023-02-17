eaf4597cc994bf3fcd4028be7aad0df0c0e4e127
John Brian <njugunajb96@gmail.com>
2023-02-17 03:05:13 +0300

    trip details screen

--- lib/globals/components/global_screens.dart
+++ lib/globals/components/global_screens.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import "package:go/screens/screens_barrel.dart"
-    show ProfileScreen;
+    show ProfileScreen, TripDetailsScreen;
 
 List<Widget> goPages = <Widget>[
+  TripDetailsScreen(),
   const ProfileScreen(),
-   Container(color: Colors.lightBlueAccent,),
+
   Container(
     color: Colors.greenAccent,
   ),
